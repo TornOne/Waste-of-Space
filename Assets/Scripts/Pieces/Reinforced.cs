7030aef5c679ae3a1bb40c089f5308d78e097003
TornOne <ott.adermann@gmail.com>
2020-04-19 12:37:35 +0300

    Can rotate pieces before placing

--- Assets/Scripts/Pieces/Reinforced.cs
+++ Assets/Scripts/Pieces/Reinforced.cs
@@ -3,4 +3,9 @@
 	public int reinforcedDamageReceived = 1;
 
 	public override void GetHit(int direction) => Health -= hasReinforced[direction] ? reinforcedDamageReceived : (hasConnector[direction] ? connectorDamageReceived : deadEndDamageReceived);
+
+	public override void Rotate(bool clockwise) {
+		base.Rotate(clockwise);
+		RotateSlots(hasReinforced, clockwise);
+	}
 }
