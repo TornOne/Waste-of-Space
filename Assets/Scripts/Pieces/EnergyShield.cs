30309ae9247fd6de562ad1f248b4fd3791bf6a6c
TornOne <ott.adermann@gmail.com>
2020-04-19 10:53:06 +0300

    Piece logic prefabs

--- Assets/Scripts/Pieces/EnergyShield.cs
+++ Assets/Scripts/Pieces/EnergyShield.cs
@@ -1,3 +1,9 @@
 ﻿public class EnergyShield : Piece {
 	public bool[] hasEnergyShield;
+	public int disabledEnergyShieldDamageReceived = 2;
+
+	public override void GetHit(int direction) {
+		//TODO: Energy calculations
+		Health -= hasEnergyShield[direction] ? disabledEnergyShieldDamageReceived : (hasConnector[direction] ? connectorDamageReceived : deadEndDamageReceived);
+	}
 }
--- Assets/Scripts/Pieces/Reinforced.cs
+++ Assets/Scripts/Pieces/Reinforced.cs
@@ -1,3 +1,8 @@
 ﻿public class Reinforced : Piece {
 	public bool[] hasReinforced;
+	public int reinforcedDamageReceived = 1;
+
+	public override void GetHit(int direction) {
+		Health -= hasReinforced[direction] ? reinforcedDamageReceived : (hasConnector[direction] ? connectorDamageReceived : deadEndDamageReceived);
+	}
 }
