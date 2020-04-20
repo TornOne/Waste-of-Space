fb7ebe7924562b27244b617d75d678a7781ec214
TornOne <ott.adermann@gmail.com>
2020-04-21 00:37:59 +0300

    I've no idea what I've done anymore

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public void LoadScene(int sceneNumber) => SceneManager.LoadScene(sceneNumber);
+
+	public void Quit() => Application.Quit(0);
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -8,12 +8,8 @@ public class UIController : MonoBehaviour {
 	}
 
 	public Image energyBar;
-	public Text energyText;
 	public float Energy {
-		set {
-			energyText.text = $"{value:0} Energy";
-			energyBar.fillAmount = 1 - 100 / (value + 100);
-		}
+		set => energyBar.fillAmount = 1 - 100 / (value + 100);
 	}
 
 	void Awake() {
