8b31eaa432e57da3d6b52cf54aab8ce3575c9037
TornOne <ott.adermann@gmail.com>
2020-04-20 15:51:55 +0300

    The sun slowly revolves around your ship

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LightController : MonoBehaviour {
+	public float revolutionDuration;
+	float spinSpeed;
+
+	void Start() => spinSpeed = 1 / revolutionDuration;
+
+	void Update() => transform.localEulerAngles += new Vector3(0, spinSpeed);
+}
